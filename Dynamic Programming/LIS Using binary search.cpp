ca9eec09a2ec192a8e6b0ef6fe6313961878a477
Devanshi Kapla <89089904+devanshikapla@users.noreply.github.com>
2023-06-26 19:58:56 +0530

    Create LIS Using binary search.cpp

---
+++ Dynamic Programming/LIS Using binary search.cpp
@@ -0,0 +1,22 @@
+vector<int> temp;
+    temp.push_back(arr[0]);
+    
+    int len = 1;
+    
+    for(int i=1; i<n; i++){
+        if(arr[i]>temp.back()){
+           // arr[i] > the last element of temp array 
+           
+           temp.push_back(arr[i]);
+           len++;
+           
+        } 
+        else{
+	// replacement step
+            int ind = lower_bound(temp.begin(),temp.end(),arr[i]) - temp.begin();
+            temp[ind] = arr[i];
+        }
+        
+    }
+    
+    return len;
